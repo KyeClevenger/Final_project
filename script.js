236436caac9dc207079a661d74aa2ba5fa9e6094
Kye Clevenger <Kyeclevenger@gmail.com>
2023-06-29 12:08:01 -0600

    second  commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function scrollWin1() {
+
+    window.scrollTo(0,0);
+  }
+
+function scrollWin2() {
+
+    window.scrollTo(0,500);
+  }
+
+  function scrollWin3() {
+
+    window.scrollTo(0,1200);
+  }
